a2cd69cb7ff13a37e46a48a34903a245ea2663fd
Bhavya Sai <bhavyasaigatta@gmail.com>
2026-05-05 23:23:25 +0530

    fixed render port issue

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,8 @@ app.get("/", (req, res) => {
 app.use("/tasks", taskRoutes);
 app.use("/auth", authRoutes);
 // ✅ Start server
-app.listen(5000, () => {
-  console.log("Server started on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
